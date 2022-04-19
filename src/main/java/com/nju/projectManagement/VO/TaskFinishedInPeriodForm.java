6259ad12d01d4d5433a6b9068c3f99dbdc71eeb8
wyx10190921 <wyx10190921@163.com>
2022-04-19 20:09:16 +0800

    fix: date format

--- src/main/java/com/nju/projectManagement/VO/TaskFinishedInPeriodForm.java
+++ src/main/java/com/nju/projectManagement/VO/TaskFinishedInPeriodForm.java
@@ -1,5 +1,6 @@
 package com.nju.projectManagement.VO;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,8 +18,10 @@ import java.util.Date;
 public class TaskFinishedInPeriodForm {
     @NotNull(message = "Empty project id")
     private Integer projectId;
+    @JsonFormat(locale = "zh", timezone="GMT+8", pattern="yyyy-MM-dd HH:mm:ss")
     @NotNull(message = "Empty start time")
     private Date startTime;
+    @JsonFormat(locale = "zh", timezone="GMT+8", pattern="yyyy-MM-dd HH:mm:ss")
     @NotNull(message = "Empty end time")
     private Date endTime;
 }
